ca9d451b0c9be845df5b9735142f6480dbdf385d
Josh Braden <josbraden@gmail.com>
2024-07-04 13:05:14 -0500

    Update modinfo, adjust armor equipment

--- src/control.lua
+++ src/control.lua
@@ -7,23 +7,29 @@ script.on_event(defines.events.on_player_created, function(event)
 
 		items = {
 			{"power-armor-mk2", 1},
-			{"construction-robot", 50},
+			{"construction-robot", 100},
 		}
 		armor = {
 			{"fusion-reactor-equipment"},
 			{"fusion-reactor-equipment"},
-			{"personal-roboport-mk2-equipment"},
-			{"personal-roboport-mk2-equipment"},
+			{"energy-shield-mk2-equipment"},
+			{"energy-shield-mk2-equipment"},
 			{"fusion-reactor-equipment"},
-			{"fusion-reactor-equipment"},
 			{"personal-roboport-mk2-equipment"},
 			{"personal-roboport-mk2-equipment"},
+			{"personal-laser-defense-equipment"},
+			{"personal-roboport-mk2-equipment"},
+			{"personal-roboport-mk2-equipment"},
+			{"personal-laser-defense-equipment"},
+			{"night-vision-equipment"},
+			{"belt-immunity-equipment"},
 			{"battery-mk2-equipment"},
 			{"battery-mk2-equipment"},
-			{"energy-shield-mk2-equipment"},
-			{"energy-shield-mk2-equipment"},
-			{"energy-shield-mk2-equipment"},
-			{"energy-shield-mk2-equipment"},
+			{"battery-mk2-equipment"},
+			{"battery-mk2-equipment"},
+			{"battery-mk2-equipment"},
+			{"battery-mk2-equipment"},
+			{"battery-mk2-equipment"},
 		}
 
 		for i, v in pairs(items) do
@@ -45,23 +51,29 @@ script.on_event(defines.events.on_cutscene_cancelled, function(event)
 
 	items = {
 		{"power-armor-mk2", 1},
-		{"construction-robot", 50},
+		{"construction-robot", 100},
 	}
 	armor = {
 		{"fusion-reactor-equipment"},
 		{"fusion-reactor-equipment"},
-		{"personal-roboport-mk2-equipment"},
-		{"personal-roboport-mk2-equipment"},
+		{"energy-shield-mk2-equipment"},
+		{"energy-shield-mk2-equipment"},
 		{"fusion-reactor-equipment"},
-		{"fusion-reactor-equipment"},
 		{"personal-roboport-mk2-equipment"},
 		{"personal-roboport-mk2-equipment"},
+		{"personal-laser-defense-equipment"},
+		{"personal-roboport-mk2-equipment"},
+		{"personal-roboport-mk2-equipment"},
+		{"personal-laser-defense-equipment"},
+		{"night-vision-equipment"},
+		{"belt-immunity-equipment"},
 		{"battery-mk2-equipment"},
 		{"battery-mk2-equipment"},
-		{"energy-shield-mk2-equipment"},
-		{"energy-shield-mk2-equipment"},
-		{"energy-shield-mk2-equipment"},
-		{"energy-shield-mk2-equipment"},
+		{"battery-mk2-equipment"},
+		{"battery-mk2-equipment"},
+		{"battery-mk2-equipment"},
+		{"battery-mk2-equipment"},
+		{"battery-mk2-equipment"},
 	}
 
 	for i, v in pairs(items) do
